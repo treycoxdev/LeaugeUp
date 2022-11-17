f5d45246a13018fd958b0c2f29f33a690d1a73c4
Trey Cox <tcollincox@gmail.com>
2022-11-17 18:08:05 -0500

    Test Class

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PickleballAPIv3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> test()
+        {
+            return Ok("Test");
+        }
+    }
+}
